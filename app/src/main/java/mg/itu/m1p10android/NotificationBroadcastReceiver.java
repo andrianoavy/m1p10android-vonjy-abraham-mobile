0ec7587d335c052882a7fa4f39c0d392dd9a79d8
Vonjy55 <vonjitahinaranjelison@gmail.com>
2023-08-07 09:07:48 +0300

    correction couleur

--- app/src/main/java/mg/itu/m1p10android/NotificationBroadcastReceiver.java
+++ app/src/main/java/mg/itu/m1p10android/NotificationBroadcastReceiver.java
@@ -11,7 +11,7 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(@NonNull Context context, Intent intent) {
         String title = "Madagascar tourisme";
-        String message = "VVous trouverez les sites touristiques proches de chez vous.";
+        String message = "Vous trouverez les sites touristiques proches de chez vous.";
         long durationInMillis = 180000; // Durée en millisecondes (1 minute dans cet exemple).
 
         LocalNotificationManager notificationManager = new LocalNotificationManager(context);
